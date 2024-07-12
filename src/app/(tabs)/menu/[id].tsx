77174ab7c92bf913a6a4509c322b6c8787e43235
Kanishk Shukla <main.shubhshukla@gmail.com>
2024-07-12 14:12:49 +0530

    Vini se dosti ho gyi

--- src/app/(tabs)/menu/[id].tsx
+++ src/app/(tabs)/menu/[id].tsx
@@ -1,18 +1,20 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { useLocalSearchParams, Stack } from 'expo-router'
+import { View, Text } from "react-native";
+import React from "react";
+import { useLocalSearchParams, Stack } from "expo-router";
 
 const ProductDetailsScreen = () => {
   const { id } = useLocalSearchParams();
 
   return (
     <View>
-      <Stack.Screen options = {{
-        title: `Product Details: ${id}`  // dynamically change title based on id
-      }} />
-      <Text>ProductDetailsScreen : {id}</Text>
+      <Stack.Screen
+        options={{
+          title: `Product Details: ${id}`, // dynamically change title based on id
+        }}
+      />
+      <Text>ProductDetailsScreen- : {id}</Text>
     </View>
-  )
-}
+  );
+};
 
-export default ProductDetailsScreen
+export default ProductDetailsScreen;

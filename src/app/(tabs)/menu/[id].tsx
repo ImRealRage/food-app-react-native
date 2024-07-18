e598191e54b8d6b5ef844b6427c6df420b69b0a6
Kanishk Shukla <main.shubhshukla@gmail.com>
2024-07-18 09:54:07 +0530

    Removed styling element from images

--- src/app/(tabs)/menu/[id].tsx
+++ src/app/(tabs)/menu/[id].tsx
@@ -1,20 +1,40 @@
-import { View, Text } from "react-native";
+import { View, Text, Image, StyleSheet } from "react-native";
 import React from "react";
 import { useLocalSearchParams, Stack } from "expo-router";
+import products from "@/assets/data/products";
+import { defaultPizzaImage } from "@/src/components/ProductListItem";
 
 const ProductDetailsScreen = () => {
   const { id } = useLocalSearchParams();
 
+  const product = products.find((p) => p.id.toString() === id);
+
+  if (!product) {
+    return <Text>Product not found</Text>;
+  }
+
   return (
-    <View>
+    <View style={styles.container}>
       <Stack.Screen
         options={{
-          title: `Product Details: ${id}`, // dynamically change title based on id
+          title: product.name,
         }}
       />
-      <Text>ProductDetailsScreen- : {id}</Text>
+
+      <Image source={{ uri: product.image || defaultPizzaImage }} />
+
+      <Text style={{ fontSize: 20 }}>ProductDetailsScreen : {id}</Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {},
+  image: {
+    width: 200,
+    height: 200,
+  },
+  price: {},
+});
 
 export default ProductDetailsScreen;

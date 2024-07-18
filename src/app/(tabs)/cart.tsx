ca6c2655d92b89d0612383a733e9f162d5fcf94c
Kanishk Shukla <main.shubhshukla@gmail.com>
2024-07-18 15:06:50 +0530

    working on cart

--- src/app/(tabs)/cart.tsx
+++ src/app/(tabs)/cart.tsx
@@ -1,10 +1,16 @@
-import { View, Text } from "react-native";
+import { View, Text, Platform } from "react-native";
 import React from "react";
+import { StatusBar } from "expo-status-bar";
+import { useCart } from "@/src/providers/CartProvider";
 
 const CartScreen = () => {
+  const { items } = useCart();
+
   return (
     <View>
-      <Text>CartScreen</Text>
+      <Text>Cart itms length : {items.length}</Text>
+
+      <StatusBar style={Platform.OS === "ios" ? "light" : "auto"} />
     </View>
   );
 };
